a5cebe6505ed2bad5d949f5b6f0d09023194e7ff
ricky martinez <rickyjmartinez2@gmail.com>
2023-09-29 16:16:24 -0400

    add login,logout, signup

--- src/Content.jsx
+++ src/Content.jsx
@@ -1,6 +1,14 @@
+import { Signup } from "./Signup"
+import { Login } from "./Login" 
+import { LogoutLink } from "./LogoutLink"
+
 export function Content() {
   return (
     <div>
+      < Signup /> 
+      < Login />
+      < br/> 
+      < LogoutLink />
       <h1>Welcome to React!</h1>
     </div>
   )
